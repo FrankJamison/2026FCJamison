16b606b5a2ab133486949d098da6c8d11bc552a4
FrankJamison <frank@frankjamison.com>
2026-01-08 20:47:11 -0800

    added 2006 frankjamison.com project and blog piece

--- homeViews.py
+++ homeViews.py
@@ -337,3 +337,9 @@ def project_file(project_slug: str, filename: str):
 @app.route('/portfolio/2006AnalogClockProject/')
 def portfolio2006AnalogClockProject():
     return redirect(url_for('project_index', project_slug='2006AnalogClockProject'), code=301)
+
+
+@app.route('/portfolio/2006FrankJamison/index.html')
+@app.route('/portfolio/2006FrankJamison/')
+def portfolio2006FrankJamison():
+    return redirect(url_for('project_index', project_slug='2006FrankJamison'), code=301)
